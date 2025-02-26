a9b0f6a09517dace29d735430be29d2f19914e5c
Bogdan Randjelovic <bogdan@openinnovation.me>
2025-02-26 14:43:59 +0100

    fixing og:image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,13 @@
     <meta name="twitter:title" content="SRPSKA TENIS LIGA"/>
     <meta name="twitter:description" content="SRPSKA TENIS LIGA"/>
     <meta name="twitter:image:src"
-          href="{{ asset('/social-media-preview.png') }}"/>
+          content="{{ asset('/social-media-preview.png') }}"/>
     <meta name="twitter:image:alt" content="srpskatenisliga.rs"/>
     <!-- Open Graph data -->
     <meta property="og:url" content="https://srpskatenisliga.rs"/>
     <meta property="og:title" content="SRPSKA TENIS LIGA"/>
     <meta property="og:image"
-          href="{{ asset('/social-media-preview.png') }}"/>
+          content="{{ asset('/social-media-preview.png') }}"/>
     <meta property="og:description" content="SRPSKA TENIS LIGA"/>
     <meta property="og:site_name" content="srpskatenisliga.rs"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
