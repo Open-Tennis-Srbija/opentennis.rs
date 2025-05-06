eaefd17a4b771e189862cb1a2d2c4b06e74a471e
Bogdan Randjelovic <bogdan@openinnovation.me>
2025-05-06 17:53:38 +0200

    Added better sorting

--- app/Http/Controllers/CourtsController.php
+++ app/Http/Controllers/CourtsController.php
@@ -8,7 +8,7 @@ use App\Models\Court;
 class CourtsController extends Controller
 {
     public static function getCourts(){
-        $courts = Court::where('id', '>', 1)->orderBy('name')->get();
+        $courts = Court::where('id', '>', 1)->orderByRaw('LOWER(name)')->get();
         return $courts;
     }
 
