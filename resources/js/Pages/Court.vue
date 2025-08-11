4c9dac381d5fc5b90cacc9e702ce5fc81ef12e45
Bogdan Randjelovic <bogdan@openinnovation.me>
2025-08-11 14:29:16 +0200

    Fixed alignment for add match button on courts with no info

--- resources/js/Pages/Court.vue
+++ resources/js/Pages/Court.vue
@@ -126,7 +126,7 @@ function containsGreek(text) {
                 </a>
             </div>
 		</p>
-        <a class="add-button align-top-mobile" :href="`/dodaj?court=${props.court_id}`">
+        <a class="add-button align-top-mobile" :class="{ 'mt-30': !court.link && !court.location && !court.phone }" :href="`/dodaj?court=${props.court_id}`">
             Dodaj meč
         </a>
 
