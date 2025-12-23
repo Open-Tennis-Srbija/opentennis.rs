2f3703f4548bac56469140c4654997b34ca7e35e
Bogdan Randjelovic <bogdan@openinnovation.me>
2025-12-23 09:48:18 +0100

    Rebranding pt 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,12 +10,12 @@
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:title" content="Crazy Pizza - Open Tennis Srbija"/>
       <meta name="twitter:description" content="BESPLATNA PICA ZA SVAKOGA SA 1,000 I VIŠE POENA"/>
-      <meta name="twitter:image:src" content="{{ asset('/promo/crazy-pizza/crazy-pizza-open-tennis-srbija.png') }}"/>
+      <meta name="twitter:image:src" content="{{ asset('/promo/crazy-pizza/ots-crazy-pizza.png') }}"/>
       <meta name="twitter:image:alt" content="Crazy Pizza"/>
       <!-- Open Graph data -->
       <meta property="og:url" content="https://opentennis.rs/crazy-pizza"/>
       <meta property="og:title" content="Crazy Pizza - Open Tennis Srbija"/>
-      <meta property="og:image" content="{{ asset('/promo/crazy-pizza/crazy-pizza-open-tennis-srbija.png') }}"/>
+      <meta property="og:image" content="{{ asset('/promo/crazy-pizza/ots-crazy-pizza.png') }}"/>
       <meta property="og:description" content="BESPLATNA PICA ZA SVAKOGA SA 1,000 I VIŠE POENA"/>
       <meta property="og:site_name" content="opentennis.rs"/>
     @else
@@ -23,12 +23,12 @@
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:title" content="Open Tennis Srbija"/>
       <meta name="twitter:description" content="svi teniseri, lige i turniri, tereni, i mečevi na jednom mestu"/>
-      <meta name="twitter:image:src" content="{{ asset('/stl-cover.png') }}"/>
+      <meta name="twitter:image:src" content="{{ asset('/ots-social-media-share.png') }}"/>
       <meta name="twitter:image:alt" content="opentennis.rs"/>
       <!-- Open Graph data -->
       <meta property="og:url" content="https://opentennis.rs"/>
       <meta property="og:title" content="Open Tennis Srbija"/>
-      <meta property="og:image" content="{{ asset('/stl-cover.png') }}"/>
+      <meta property="og:image" content="{{ asset('/ots-social-media-share.png') }}"/>
       <meta property="og:description" content="svi teniseri, lige i turniri, tereni, i mečevi na jednom mestu"/>
       <meta property="og:site_name" content="opentennis.rs"/>
     @endif
