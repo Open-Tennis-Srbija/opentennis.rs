3c416deaf3eb024000ee8ea37121ea3406197a0e
Bogdan Randjelovic <bogdan@openinnovation.me>
2026-04-20 17:15:28 +0200

    Links

--- resources/js/Pages/static/Volonteri.vue
+++ resources/js/Pages/static/Volonteri.vue
@@ -18,13 +18,13 @@ onMounted(async () => {
             Zato predlažemo da klubovi informišu svoje članove da mogu da koriste Ligu.
 </p> -->
         <p>
-            <Link href="/o-nama">Open Tennis Srbija</Link> želi da skupi sve podatke o srpskom tenisu na jednom mestu. Zasniva se na volonterskom radu pojedinaca koji pronalaze, organizuju i dodaju teniske podatke na sajt. Taj posao zahteva znanje, vreme i fokus.
+            <Link href="/o-nama">Open Tennis Srbija</Link> (OTS) želi da skupi sve podatke o srpskom tenisu na jednom mestu. Zasniva se na volonterskom radu pojedinaca koji pronalaze, organizuju i dodaju teniske podatke na <Link href="/o-nama">sajt</Link>. Taj posao zahteva znanje, vreme i fokus.
         </p>
         <p>
            Svako može da bude <Link href="/o-nama">OTS</Link> volonter. Godište i lokacija nisu važni. Idealno je za srednjoškolce i studente.
         </p>
         <p>
-           Nemamo radno vreme niti rad na lokaciji. Volonteri mogu da rade kada žele i odakle žele preko svog mobilnog ili kompjutera. Obaveze su nalaženje, organizovanje i objavljivanje teniskih podataka srpskih tenisera i teniskih događaja u Srbiji na opentennis.rs. Takođe, volonteri mogu da budu odgovorni za njihove klubove, turnire, lige, igrače i lokacije.
+           Nemamo radno vreme niti rad na lokaciji. Volonteri mogu da rade kada žele i odakle žele preko svog mobilnog ili kompjutera. Obaveze su nalaženje, organizovanje i objavljivanje teniskih podataka srpskih tenisera i teniskih događaja u Srbiji na opentennis.rs. Takođe, volonteri mogu da budu odgovorni za njihove klubove, <Link href="/turniri">turnire</Link>, <Link href="/lige">lige</Link>, igrače i lokacije.
         </p>
         <p>
             Kao <Link href="/o-nama">OTS</Link> volonter razvićete menadžment i digitalne veštine, a i ostvarićete nove kontakte u tenisu i šire.
